4acf2da785fef7a6a001e8b209e6eceaf42e4829
Trqkhanh17 <khanhtranquoc44@gmail.com>
2025-08-01 01:55:03 +0700

    add debug,Create user,find users

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { Prop } from "@nestjs/mongoose";
+import { IsEmail, IsNotEmpty, IsPhoneNumber } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty({message:"tên không được để trống"})
+    name:string;
+
+    @IsNotEmpty({message:"email không được để trống"})
+    @IsEmail({},{message:"email không đúng định dạng"})
+    email:string;
+
+    @IsNotEmpty({message:"password không được để trống"})
+    password: string;
+
+    @IsNotEmpty({message:"số điện thoại không được để trống"})
+    phone:string;
+    
+    address:string;
+
+    image:string;
+}
